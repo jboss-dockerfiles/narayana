27ba0888013203fc5843669d398e2e49cf7ec9f6
Ondra Chaloupka <ochaloup@redhat.com>
2019-03-14 17:25:29 +0100

    [JBTM-3118] adding ipv4 stack as prefered one

--- lra/lra-coordinator/run-lra-coordinator.sh
+++ lra/lra-coordinator/run-lra-coordinator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# settings taken from redhat-jdk/redhat-jdk image
-export JAVA_OPTS=${JAVA_OPTS:-"-XX:+UseParallelOldGC -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MaxMetaspaceSize=100m -XX:+ExitOnOutOfMemoryError"}
+# settings taken from redhat-jdk/redhat-jdk image + adding prefered IPv4 stack
+export JAVA_OPTS=${JAVA_OPTS:-"-XX:+UseParallelOldGC -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MaxMetaspaceSize=100m -XX:+ExitOnOutOfMemoryError -Djava.net.preferIPv4Stack=true"}
 
 # showing the java settings at the start of the lra coordinator
 EXEC="java $JAVA_OPTS -Dswarm.logging=$LOG_LEVEL -Dswarm.transactions.object-store-path="$OBJECT_STORE_PATH" -jar"
